4c0ae4346d279af585001b83dc5b92aff3f0a36c
Yixuan Li <lxd520ANNA@gmail.com>
2022-10-08 18:26:03 -0400

    heroku

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ const detector = require("./routes/detector.routes");
 app.use(cors());
 app.use(express.json());
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+    );
     next();
 });
 
