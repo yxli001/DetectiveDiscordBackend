34c35932c5e4f19865d9d76920059093916a71fb
Yixuan Li <lxd520ANNA@gmail.com>
2022-10-08 18:13:43 -0400

    heroku fix

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const detector = require("./routes/detector.routes");
 app.use(cors());
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
 app.use("/api/detector", detector);
 
 app.listen(PORT, () => {
