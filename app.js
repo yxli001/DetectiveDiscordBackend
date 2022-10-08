3d224fdea13fb9f5a7b9fd0bd7e576025b1a7321
Yixuan Li <lxd520ANNA@gmail.com>
2022-10-08 18:21:47 -0400

    heroku fix again

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
